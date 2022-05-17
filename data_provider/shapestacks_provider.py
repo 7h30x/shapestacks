e69e847f6bdb14a28920c86f4236913859a9569f
Patrick Buckley <u7122195@anu.edu.au>
2022-05-17 14:19:37 +1000

    Print Filenames

--- data_provider/shapestacks_provider.py
+++ data_provider/shapestacks_provider.py
@@ -44,7 +44,6 @@ def _get_filenames_with_labels(mode, data_dir, split_dir):
     raise ValueError("Mode %s is not supported!" % mode)
   with open(scenario_list_file) as f:
     scenario_list = f.read().split('\n')
-    print(scenario_list)
     scenario_list.pop()
 
   filenames = []
@@ -62,6 +61,7 @@ def _get_filenames_with_labels(mode, data_dir, split_dir):
         os.listdir(scenario_dir)):
       filenames.append(os.path.join(scenario_dir, img_file))
       labels.append(label)
+  print(filenames)
 
   return filenames, labels
 
