6f006fa3b27f48cd098afb4dba4af233cf1d3e05
Patrick Buckley <u7122195@anu.edu.au>
2022-05-24 05:04:49 +1000

    Remove Estimator

--- intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
+++ intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
@@ -117,15 +117,15 @@ def analyse_checkpoint(dir_snapshot, name_snapshot, unparsed_argv):
   num = 0
   i = scenario_list[0]#for num, i in enumerate(scenario_list):
   features, labels = shapestacks_input_fn('eval', FLAGS.data_dir, FLAGS.split_name, FLAGS.batch_size, i, num, FLAGS.epochs_per_eval, FLAGS.n_prefetch, FLAGS.augment, FLAGS.angle_nums)
-  c = inception_v4_base(features)
-  #a = inception_v4(features, 1, False)
+  #c = inception_v4_base(features)
+  a = inception_v4(features, 1, False, reuse=True)
   sess = tf.compat.v1.Session()
   sess.run(c)
   tf.reset_default_graph()
   num = 1
   features, labels = shapestacks_input_fn('eval', FLAGS.data_dir, FLAGS.split_name, FLAGS.batch_size, i, num, FLAGS.epochs_per_eval, FLAGS.n_prefetch, FLAGS.augment, FLAGS.angle_nums)
-  #b = inception_v4(features, 1, False)
-  d = inception_v4_base(features)
+  b = inception_v4(features, 1, False, reuse=True)
+  #d = inception_v4_base(features)
   sess1 = tf.compat.v1.Session()
   sess1.run(d)
   print(a)
