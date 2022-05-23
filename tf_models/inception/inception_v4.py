270d3289c9d10358c1a0d0693fb4d7f059ca93ff
Patrick Buckley <u7122195@anu.edu.au>
2022-05-24 03:19:42 +1000

    Remove Estimator

--- tf_models/inception/inception_v4.py
+++ tf_models/inception/inception_v4.py
@@ -37,16 +37,16 @@ def block_inception_a(inputs, scope=None, reuse=None):
   with slim.arg_scope([slim.conv2d, slim.avg_pool2d, slim.max_pool2d],
                       stride=1, padding='SAME'):
     with tf.variable_scope(scope, 'BlockInceptionA', [inputs], reuse=reuse):
-      with tf.variable_scope('Branch_0'):
+      with tf.variable_scope('Branch_0',reuse=reuse):
         branch_0 = slim.conv2d(inputs, 96, [1, 1], scope='Conv2d_0a_1x1')
-      with tf.variable_scope('Branch_1'):
+      with tf.variable_scope('Branch_1',reuse=reuse):
         branch_1 = slim.conv2d(inputs, 64, [1, 1], scope='Conv2d_0a_1x1')
         branch_1 = slim.conv2d(branch_1, 96, [3, 3], scope='Conv2d_0b_3x3')
-      with tf.variable_scope('Branch_2'):
+      with tf.variable_scope('Branch_2',reuse=reuse):
         branch_2 = slim.conv2d(inputs, 64, [1, 1], scope='Conv2d_0a_1x1')
         branch_2 = slim.conv2d(branch_2, 96, [3, 3], scope='Conv2d_0b_3x3')
         branch_2 = slim.conv2d(branch_2, 96, [3, 3], scope='Conv2d_0c_3x3')
-      with tf.variable_scope('Branch_3'):
+      with tf.variable_scope('Branch_3',reuse=reuse):
         branch_3 = slim.avg_pool2d(inputs, [3, 3], scope='AvgPool_0a_3x3')
         branch_3 = slim.conv2d(branch_3, 96, [1, 1], scope='Conv2d_0b_1x1')
       return tf.concat(axis=3, values=[branch_0, branch_1, branch_2, branch_3])
