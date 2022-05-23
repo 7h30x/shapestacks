e07a86bdf0e13aff8c60561e00430bbb52f41139
Patrick Buckley <u7122195@anu.edu.au>
2022-05-23 12:55:25 +1000

    Real only

--- intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
+++ intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
@@ -129,8 +129,7 @@ def analyse_checkpoint(dir_snapshot, name_snapshot, unparsed_argv):
   
   total = 0.0
   for i, pred in enumerate(prediction_mean):
-    if pred.round() == labels[i]:
-      total += 1.0
+    total += (pred - labels[i]) ** 2
   total = total / len(prediction_mean)
   print(total)
 
