b250f9ec7c3976f2e127bd930bfeea8a0b695431
Patrick Buckley <u7122195@anu.edu.au>
2022-05-24 09:07:03 +1000

    Remove Estimator

--- intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
+++ intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
@@ -156,7 +156,7 @@ def analyse_checkpoint(dir_snapshot, name_snapshot, unparsed_argv):
     #labels.append(test_results['label'])
   total = 0.0
   for i, pred in enumerate(prediction_mean):
-    total += (pred[0] - labels[i]) ** 2
+    total += (pred - labels[i]) ** 2
   total = total / len(prediction_mean)
   print(total)
 
