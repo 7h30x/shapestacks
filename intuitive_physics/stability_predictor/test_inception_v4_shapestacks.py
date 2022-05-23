705d2ac63728ea464e8e4f7a757eeaa8658f6950
Patrick Buckley <u7122195@anu.edu.au>
2022-05-23 13:21:11 +1000

    Real only

--- intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
+++ intuitive_physics/stability_predictor/test_inception_v4_shapestacks.py
@@ -126,12 +126,13 @@ def analyse_checkpoint(dir_snapshot, name_snapshot, unparsed_argv):
     prediction_mean.append(test_results['pred_mean'])
     print(test_results['label'])
     labels.append(test_results['label'])
-  
-  total = 0.0
-  for i, pred in enumerate(prediction_mean):
-    total += (pred - labels[i]) ** 2
-  total = total / len(prediction_mean)
-  print(total)
+  prediction_mean.sort()
+  print(prediction_mean[round(len(prediction_mean) / 2)
+  #total = 0.0
+  #for i, pred in enumerate(prediction_mean):
+  #  total += (pred - labels[i]) ** 2
+  #total = total / len(prediction_mean)
+  #print(total)
 
 #   # evaluate the model on real data
 #   if FLAGS.real_data_dir != '':
