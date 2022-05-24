b289c1f81ebc82e2c1a8da53e3832ac6de5bcd3b
Patrick Buckley <u7122195@anu.edu.au>
2022-05-24 10:23:50 +1000

    Remove Estimator

--- data_provider/shapestacks_provider.py
+++ data_provider/shapestacks_provider.py
@@ -256,7 +256,7 @@ def shapestacks_input_fn(
 
   # parse data from files and apply pre-processing
   filenames = tf.constant(filenames)
-  images = filenames.map(_parse_record)
+  images = tf.map_fn(_parse_record,filenames)
   if augment != [] and mode == 'train':
     dataset = dataset.map(lambda feature, label: _augment(feature, label, augment))
   if 'subtract_mean' in augment:
